cc0b969fffea6c19913ac9b1e6b05af39bca9e19
Kuba Kwasiborski <kubaczek@gmail.com>
2013-01-17 17:32:43 +0000

    this might work

--- js/swipe.js
+++ js/swipe.js
@@ -1,14 +1,33 @@
-function caluculateAngle() {
-		var X = startX-curX;
-		var Y = curY-startY;
+var Toucher = {
+    startX : 0,
+    startY : 0,
+    currentX : 0,
+    currentY : 0,
+    touchStart : function (event) {
+        if (event.touches.length != 1) {
+            return false;
+        }
+        Toucher.startX = event.touches[0].pageX;
+        Toucher.startY = event.touches[0].pageY;
+    },
+    touchMove : function (event) {
+        if (event.touches.length != 1) {
+            Toucher.touchCancel();
+        }
+        Toucher.currentX = event.touches[0].pageX;
+        Toucher.currentY = event.touches[0].pageY;
+    },
+    touchEnd : function (event) {
+        var X = Toucher.startX-Toucher.currentX;
+		var Y = Toucher.currentY-Toucher.startY;
 		var Z = Math.round(Math.sqrt(Math.pow(X,2)+Math.pow(Y,2))); //the distance - rounded - in pixels
 		var r = Math.atan2(Y,X); //angle in radians (Cartesian system)
-		swipeAngle = Math.round(r*180/Math.PI); //angle in degrees
-		if ( swipeAngle < 0 ) { swipeAngle =  360 - Math.abs(swipeAngle); }
-	}
-	
-	function determineSwipeDirection() {
-		if ( (swipeAngle <= 45) && (swipeAngle >= 0) ) {
+		var swipeAngle = Math.round(r*180/Math.PI); //angle in degrees
+		if ( swipeAngle < 0 ) { 
+            swipeAngle =  360 - Math.abs(swipeAngle);
+        }
+        var swipeDirection = '';
+        if ( (swipeAngle <= 45) && (swipeAngle >= 0) ) {
 			swipeDirection = 'left';
 		} else if ( (swipeAngle <= 360) && (swipeAngle >= 315) ) {
 			swipeDirection = 'left';
@@ -19,4 +38,16 @@ function caluculateAngle() {
 		} else {
 			swipeDirection = 'up';
 		}
-	}
+        console.log(swipeDirection);
+    },
+    touchCancel : function (event) {
+        Toucher.startX = Toucher.startY = Toucher.currentX = Toucher.currentY = 0;
+    }
+};
+
+$(function () {
+    $("#touchArea").bind('touchstart', Toucher.touchStart);
+    $("#touchArea").bind('touchend', Toucher.touchEnd);
+    $("#touchArea").bind('touchmove', Toucher.touchMove);
+    $("#touchArea").bind('touchcancel', Toucher.touchCancel);
+});
